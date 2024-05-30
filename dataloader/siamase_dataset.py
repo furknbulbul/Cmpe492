58bcdc88e707988e1da832f9d14c0484e7a2447a
Furkan Bülbül <furknblbl@gmail.com>
2024-05-30 22:56:46 +0300

    directory name change

---
+++ dataloader/siamase_dataset.py
@@ -0,0 +1,12 @@
+
+
+class SiamaseDataset(Dataset):
+    def __init__(self, data, transform=None):
+        self.data = data
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        pass
